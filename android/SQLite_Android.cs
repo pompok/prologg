26d6c3348b2ed466ed473aadd93c6e34f481dc5f
pompok <40019610+pompok@users.noreply.github.com>
2018-06-08 11:02:22 +0300

    Add files via upload

---
+++ android/SQLite_Android.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using prilogg.Data;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using Xamarin.Forms;
+using prilogg.Droid.Data;
+
+[assembly:Dependency(typeof (SQLite_Android))]
+    
+namespace prilogg.Droid.Data
+{
+   public class SQLite_Android :ISQLite
+    {
+        public SQLite_Android() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "TestDB.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFileName);
+            var conn = new SQLite.SQLiteConnection(path);
+
+            return conn;
+        }
+    }
+}
