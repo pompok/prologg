625d70ed3d785145601d949feec5cd6775b05d09
pompok <40019610+pompok@users.noreply.github.com>
2018-06-08 10:53:38 +0300

    Add files via upload

---
+++ Data/INetworkConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prilogg.Data
+{
+  public interface INetworkConnection
+    {
+        bool IsConnected { get; }
+        void CheckNetworkConnection();
+    }
+}
